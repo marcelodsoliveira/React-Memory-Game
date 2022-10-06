6fbd04ed432d5dad3a1eb9322dc5eb5fe0cb5a82
Marcelo dos Santos Oliveira <oliveira.lnx@gmail.com>
2022-10-06 13:17:10 -0300

    First commit of this project

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import * as C from './App.styles';
+import logoImage from './assets/devmemory_logo.png';
+import RestartIcon from './svgs/restart.svg'
+import { Button } from './components/Button';
+import { InfoItem } from './components/InfoItem';
+
+
+const App = () => {
+
+  const resetAndCreateGrid = () => {
+
+  }
+
+  return (
+    <C.Container>
+      <C.Info>
+        <C.LogoLink>
+          <img src={logoImage} width="200" alt="" />
+        </C.LogoLink>
+
+        <C.InfoArea>
+          <InfoItem label="Temp" value="00:00" />
+          <InfoItem label="Movimentos" value="0" />
+        </C.InfoArea>
+
+        <Button label="Reiniciar" icon={RestartIcon} onClick={resetAndCreateGrid} />
+      </C.Info>
+      <C.GridArea>
+        ...
+      </C.GridArea>
+    </C.Container>
+  );
+}
+
+export default App;
