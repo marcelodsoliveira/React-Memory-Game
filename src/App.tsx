72b34eb307f799fdb91d7cf2a821a2e00d1f68ab
Marcelo S Oliveira <oliveira.lnx@gmail.com>
2022-10-14 16:25:47 -0300

    Grid configuration

--- src/App.tsx
+++ src/App.tsx
@@ -32,12 +32,20 @@ const App = () => {
       item: null, shown: false, permanentShown: false
     });
     // 2.2 - preencher o grid
-
+    for(let w = 0; w < 2; w++) {
+      for(let i = 0; i < items.length; i++) {
+        let pos = -1;
+        while(pos < 0 || tmpGrid[pos].item !== null) {
+          pos = Math.floor(Math.random() * (items.length * 2));
+        }
+        tmpGrid[pos].item = i;
+      }
+    }
     // 2.3 - jogar no state
     setGridItems(tmpGrid);
-
+   
     // passo 3 - começar o jogo
-    setPlaying(true);
+    setPlaying(true); 
   }
 
   return (
