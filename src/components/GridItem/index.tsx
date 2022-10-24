9a99a28315734bb4d3c1ec040f62d77da4bdac5d
Marcelo dos Santos Oliveira <oliveira.lnx@gmail.com>
2022-10-24 16:56:59 -0300

    Adding grid items

---
+++ src/components/GridItem/index.tsx
@@ -0,0 +1,18 @@
+import { GridItemType } from '../../types/GridItemType';
+import * as C from './styles';
+import b7svg from '../../svgs/b7svg.svg';
+
+type Props = {
+  item: GridItemType,
+  onClick: () => void
+}
+
+export const GridItem = ({ item, onClick }: Props) => {
+  return (
+    <C.Container onClick={onClick}>
+      {item.permanentShown === false && item.shown === false &&
+        <C.Icon src={b7svg} alt="" />
+      }
+    </C.Container>
+  );
+}
