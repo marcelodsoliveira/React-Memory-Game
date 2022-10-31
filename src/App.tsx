be16cb84d6109fb49b550a41328c1f528b53993d
Marcelo dos Santos Oliveira <oliveira.lnx@gmail.com>
2022-10-31 16:11:20 -0300

    Created helpers

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { GridItem } from './components/GridItem';
 
 import { GridItemType } from './types/GridItemType';
 import { items} from './data/items';
+import { formatTimeElapsed } from './helpers/formatTimeElapsed';
 
 
 
@@ -22,6 +23,13 @@ const App = () => {
   const [gridItems, setGridItems] = useState<GridItemType[]>([]);
 
   useEffect(() => resetAndCreateGrid(), []);
+
+  useEffect(()=> {
+    const timer = setInterval(() => {
+      if(playing) setTimeElapsed(timeElapsed + 1);
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [playing, timeElapsed]);
 
   const resetAndCreateGrid = () => {
     // passo 1 - resetar o jogo
@@ -65,7 +73,7 @@ const App = () => {
         </C.LogoLink>
 
         <C.InfoArea>
-          <InfoItem label="Temp" value="00:00" />
+          <InfoItem label="Tempo" value={formatTimeElapsed(timeElapsed)} />
           <InfoItem label="Movimentos" value="0" />
         </C.InfoArea>
 
